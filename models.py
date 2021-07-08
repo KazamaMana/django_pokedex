72d27a922dcdfe9f16e421608abb07b940a42eed
Mana <umbrannarcissus@hotmail.com>
2021-07-08 02:10:53 -0400

    Implemented json parse and api request

--- models.py
+++ models.py
@@ -1,7 +1,34 @@
-# utils
-from utils import get
+import requests
+import json
+
+URL = "https://pokeapi.co/api/v2/pokemon/"
+
+
 
 def pokedex_request():
-    pokemon_json = get(url='https://pokeapi.co/api/v2/pokemon/zigzagoon/')
-    return pokemon_json
+    pkmn_name = input('Name or Pokemon ID ')
+    url_request = URL + pkmn_name
+    
+    pokemon_json = requests.get(url_request)
+    return pokemon_json.json()
 
+
+
+def json_parse(raw_data):
+    pokemon_move = [moves['move']['name'] for moves in raw_data['moves']]
+    pokemon_type = [types['type']['name'] for types in raw_data['types']]
+    pokemon_type = "/".join(pokemon_type)
+    pokemon_data ={
+        "name":'',
+        "type":'',
+        "weight":'',
+        "height":'',
+        "moves":''
+    }
+    pokemon_data['name'] = raw_data['name']
+    pokemon_data['type'] = pokemon_type
+    pokemon_data['weight'] = raw_data['weight']
+    pokemon_data['height'] = raw_data['height']
+    pokemon_data['moves'] = pokemon_move
+    
+    return pokemon_data
